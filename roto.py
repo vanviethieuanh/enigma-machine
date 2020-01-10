b71972c30314b9e2c338a43b50653b49bdfe90bc
Accodius of Van <accodius@accodius.accodiusov>
2020-01-10 23:09:03 +0700

    wiring set, fix roto rotate

--- roto.py
+++ roto.py
@@ -4,23 +4,16 @@ import terminaltables
 class Roto:
 
     def __init__(self):
-        self.pathway = list(range(0,26))
-        random.shuffle(self.pathway)
+        self.pathway = []
 
-        self.pointer = 0
-        self.printRoto()
+        rotoWiring = input("Please enter roto's Wiring: ")
+        for c in rotoWiring:
+            self.pathway.append(ord(c)-65)
 
     def rotate(self):
-        if self.pointer == 25:
-            self.pointer = 0
-            return
-        self.pointer += 1
+        self.pathway = self.pathway[1:] + self.pathway[:1]
 
     def input(self,index):
-        if index+self.pointer > 25:
-            index = index + self.pointer - 25
-        else:
-            index = index + self.pointer
         return self.pathway[index]
     
     def reverse(self,number):
@@ -33,10 +26,6 @@ class Roto:
         map = map[0:-1]
         print(map)
         pass
-
-
-
-    
 
 
 
